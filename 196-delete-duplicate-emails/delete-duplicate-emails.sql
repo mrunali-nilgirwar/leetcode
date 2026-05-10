d5764dd759b1971b5a88f0a0f9e5076c73a85bab
mrunali-nilgirwar <135994453+mrunali-nilgirwar@users.noreply.github.com>
2026-05-09 23:37:09 -0500

    Time: 839 ms (55.60%) | Memory: 0B (100.00%) - LeetSync

--- 196-delete-duplicate-emails/delete-duplicate-emails.sql
+++ 196-delete-duplicate-emails/delete-duplicate-emails.sql
@@ -1,19 +1,5 @@
 # Write your MySQL query statement below
-DELETE p1
+delete p1
 from Person p1
-join Person p2 on p1.email=p2.email
-and p1.id>p2.id
-
-
-
-
--- DELETE FROM Person
--- where id 
--- in 
--- (select email,id,
--- dense_rank() over (partition by email order by id) as rnk
--- from Person
--- where dense_rank() over (partition by email order by id) > 1
--- )
-
-
+    join Person p2 on p1.email=p2.email
+    where p1.id>p2.id
